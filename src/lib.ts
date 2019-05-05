e15891b5177a237dfa1165326b28d4177cbc2fa7
Andriy F <1040440+andriy-f@users.noreply.github.com>
2019-05-05 22:27:13 +0300

    tslint -> eslint

--- src/lib.ts
+++ src/lib.ts
@@ -1,10 +1,14 @@
 import { range } from 'rxjs'
 
-export const add = (a: number, b: number) => {
+export const add = (a: number, b: number): number => {
   return a + b
 }
 
-export const testrange = (nextCb: (i: number) => void, errCb: (error: any) => void, completeCb: () => void) => {
+export const testrange = (
+  nextCb: (i: number) => void,
+  errCb: (error: any) => void, // eslint-disable-line @typescript-eslint/no-explicit-any
+  completeCb: () => void
+): void => {
   const source = range(1, 10)
   source.subscribe({
     complete: completeCb,
--- test/lib.test.ts
+++ test/lib.test.ts
@@ -1,14 +1,14 @@
 import { add, testrange } from '../src/lib'
 
-test('adds 1 + 2 to equal 3', () => {
+test('adds 1 + 2 to equal 3', (): void => {
   expect(add(1, 2)).toBe(3)
 })
 
-test('test rxjs range', (done) => {
+test('test rxjs range', (done): void => {
   const vals: number[] = []
-  const nextCb = (val: number) => vals.push(val)
-  const errCb = (err: any) => { done(err) }
-  const completeCb = () => {
+  const nextCb = (val: number): void => { vals.push(val) }
+  const errCb = (err: any): void => { done(err) } // eslint-disable-line @typescript-eslint/no-explicit-any
+  const completeCb = (): void => {
     expect(vals).toStrictEqual([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
     done()
   }
