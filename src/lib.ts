8bf21ff2833e76f1ff8411bb1ea55967972ffdcc
Andriy F <1040440+andriy-f@users.noreply.github.com>
2019-05-05 21:42:40 +0300

    Add rxjs test

--- src/lib.ts
+++ src/lib.ts
@@ -1,3 +1,14 @@
+import { range } from 'rxjs'
+
 export const add = (a: number, b: number) => {
-    return a + b
+  return a + b
+}
+
+export const testrange = (nextCb: (i: number) => void, errCb: (error: any) => void, completeCb: () => void) => {
+  const source = range(1, 10)
+  source.subscribe({
+    complete: completeCb,
+    error: errCb,
+    next: nextCb,
+  })
 }
--- test/lib.test.ts
+++ test/lib.test.ts
@@ -1,5 +1,17 @@
-import { add } from '../src/lib'
+import { add, testrange } from '../src/lib'
 
 test('adds 1 + 2 to equal 3', () => {
   expect(add(1, 2)).toBe(3)
+})
+
+test('test rxjs range', (done) => {
+  const vals: number[] = []
+  const nextCb = (val: number) => vals.push(val)
+  const errCb = (err: any) => { done(err) }
+  const completeCb = () => {
+    expect(vals).toStrictEqual([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
+    done()
+  }
+
+  testrange(nextCb, errCb, completeCb)
 })
