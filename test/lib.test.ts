d54f577784fbfc546752a7ddfe1432769d1ba935
Andriy F <1040440+andriy-f@users.noreply.github.com>
2019-05-06 09:51:33 +0300

    fail lint

--- test/lib.test.ts
+++ test/lib.test.ts
@@ -9,7 +9,7 @@ test('test rxjs range', (done): void => {
   const nextCb = (val: number): void => { vals.push(val) }
   const errCb = (err: any): void => { done(err) } // eslint-disable-line @typescript-eslint/no-explicit-any
   const completeCb = (): void => {
-    expect(vals).toStrictEqual([1, 2, 3, 4, 5, 6, 7, 8, 9, 11])
+      expect(vals).toStrictEqual([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
     done()
   }
 
