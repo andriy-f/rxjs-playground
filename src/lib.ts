730c019ad304a6c56b67073de1f3289ab29be97b
Andriy F <1040440+andriy-f@users.noreply.github.com>
2019-05-06 11:39:42 +0300

    add more tests

--- src/lib.ts
+++ src/lib.ts
@@ -1,4 +1,5 @@
-import { range, Observable } from 'rxjs'
+import { range, of, interval, Observable } from 'rxjs'
+import { mergeMap, map, scan, filter, takeUntil, withLatestFrom } from 'rxjs/operators'
 
 export const add = (a: number, b: number): number => {
   return a + b
@@ -6,4 +7,40 @@ export const add = (a: number, b: number): number => {
 
 export const rangesample = (): Observable<number> => {
   return range(1, 10)
+}
+
+export const mergeMapSample = (): Observable<string> => {
+  const lettersSource = of('a', 'b', 'c')
+  const lettersWithNumbersSource = lettersSource.pipe(
+    mergeMap((x): Observable<string> => interval(1000).pipe(map((i): string => x + i))),
+  )
+
+  // const resultCount = lettersWithNumbersSource.pipe(scan((acc, val) => acc + '1'))
+  const resultCount = lettersWithNumbersSource.pipe(scan((acc, val): string => acc + val))
+  const fiveCountCondition = resultCount.pipe(filter((val): boolean => val.length > 1))
+
+  const res = lettersSource.pipe(takeUntil(fiveCountCondition))
+  return res
+}
+
+export const takeUntilSample = (): Observable<string> => {
+  //emit value every 1s
+  const source = interval(100)
+  //is number even?
+  const isEven = (val: number): boolean => val % 2 === 0
+  //only allow values that are even
+  const evenSource = source.pipe(filter(isEven))
+  //keep a running total of the number of even numbers out
+  const evenNumberCount = evenSource.pipe(scan((acc): number => acc + 1, 0))
+  //do not emit until 5 even numbers have been emitted
+  const fiveEvenNumbers = evenNumberCount.pipe(filter((val): boolean => val > 5))
+
+  const res = evenSource.pipe(
+    //also give me the current even number count for display
+    withLatestFrom(evenNumberCount),
+    map(([val, count]): string => `Even number (${count}) : ${val}`),
+    //when five even numbers have been emitted, complete source observable
+    takeUntil(fiveEvenNumbers)
+  )
+  return res
 }
--- test/lib.test.ts
+++ test/lib.test.ts
@@ -1,4 +1,4 @@
-import { add, rangesample } from '../src/lib'
+import { add, rangesample, mergeMapSample, takeUntilSample } from '../src/lib'
 
 test('adds 1 + 2 to equal 3', (): void => {
   expect(add(1, 2)).toBe(3)
@@ -12,6 +12,38 @@ test('test rxjs range', (done): void => {
     error: (err): void => { done(err) },
     complete: (): void => {
       expect(vals).toStrictEqual([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
+      done()
+    }
+  })
+})
+
+test('test rxjs mergeMap', (done): void => {
+  const vals: string[] = []
+
+  mergeMapSample().subscribe({
+    next: (val): void => { vals.push(val) },
+    error: (err): void => { done(err) },
+    complete: (): void => {
+      expect(vals).toStrictEqual(['a', 'b', 'c'])
+      done()
+    }
+  })
+})
+
+test('test rxjs takeUntil', (done): void => {
+  const vals: string[] = []
+
+  takeUntilSample().subscribe({
+    next: (val): void => { vals.push(val) },
+    error: (err): void => { done(err) },
+    complete: (): void => {
+      expect(vals).toStrictEqual([
+        'Even number (1) : 0',
+        'Even number (2) : 2',
+        'Even number (3) : 4',
+        'Even number (4) : 6',
+        'Even number (5) : 8',
+      ])
       done()
     }
   })
